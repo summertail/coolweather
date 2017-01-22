a976278870acb0fdcaf93813b674584439eff803
summertail <501965134@qq.com>
2017-01-22 20:41:18 +0800

    second commit

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/MainActivity.java
@@ -0,0 +1,16 @@
+package android.coolweather.com.coolweather.gson;
+
+import android.coolweather.com.coolweather.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
